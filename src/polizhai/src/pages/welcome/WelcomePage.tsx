30d27141464d6052ea001c95c230a7c4303a23ab
Dmytro <dmytropolizhai@gmail.com>
2025-06-25 01:36:18 +0300

    init react app with ts

---
+++ src/polizhai/src/pages/welcome/WelcomePage.tsx
@@ -0,0 +1,9 @@
+import './welcome-page.css'
+
+export const WelcomePage = () => {
+    return (
+      <div className="welcome-page">
+          <p className="welcome-page__title">“Make it run, make it right”</p>
+      </div>
+    );
+}
