f9d28a05515a9a63e013bb09d3cd6006a044b096
Dmytro <dmytropolizhai@gmail.com>
2025-07-15 18:46:59 +0300

    fix: save selected theme to local storage

--- website/src/app/providers/ThemeModeProvider.tsx
+++ website/src/app/providers/ThemeModeProvider.tsx
@@ -1,14 +1,32 @@
-import React, { useMemo, useState } from "react";
+import React, { useEffect, useMemo, useState } from "react";
 import type { ThemeMode } from "@shared/type/ThemeMode.ts";
 import { createTheme, ThemeProvider } from "@mui/material/styles";
+
+import { ThemeContext } from "@app/contexts/ThemeContext.tsx";
 import { Colors } from "@shared/conts/Colors.ts";
 
-import { ThemeContext } from "../contexts/ThemeContext.tsx";
 
 export const ThemeModeProvider: React.FC<{ children: React.ReactNode }> = ({ children }) => {
     const [mode, setMode] = useState<ThemeMode>('dark');
 
-    const toggleTheme = () => setMode((prev) => (prev === 'dark' ? 'light' : 'dark'));
+    useEffect(() => {
+        loadThemeMode();
+    }, [])
+
+    const toggleTheme = (): void => {
+        const newMode: ThemeMode = mode === "dark" ? "light" : "dark";
+        setMode(newMode);
+        localStorage.setItem("themeMode", newMode);
+    };
+
+    const loadThemeMode = () => {
+        const raw = localStorage.getItem("themeMode");
+        const newMode: ThemeMode = raw === "light" || raw === "dark" ? raw : "light";
+
+        setMode(newMode);
+    }
+
+
 
     const theme = useMemo(() => {
         return createTheme({
@@ -41,9 +59,10 @@ export const ThemeModeProvider: React.FC<{ children: React.ReactNode }> = ({ chi
         });
     }, [mode]);
 
+
     return (
         <ThemeContext.Provider value={{ mode, toggleTheme }}>
-    <ThemeProvider theme={theme}>{children}</ThemeProvider>
+            <ThemeProvider theme={theme}>{children}</ThemeProvider>
         </ThemeContext.Provider>
 );
 };
